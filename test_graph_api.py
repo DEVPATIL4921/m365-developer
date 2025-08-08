ef9491f03ce4494d1d3fa21c19651352a5e701e5
Jayendra Jadhav <jayendravjadhav256@gmail.com>
2025-08-08 22:33:42 +0530

    first commit

---
+++ test_graph_api.py
@@ -0,0 +1,13 @@
+# Simple script to test an API endpoint
+import requests
+
+# Using a public sample endpoint from Microsoft Graph documentation
+url = "https://graph.microsoft.com/v1.0/users?$top=5"
+
+try:
+    response = requests.get(url)
+    print("Request successful!")
+    print("Status Code:", response.status_code)
+    # print(response.json()) # You can uncomment this to see the data
+except Exception as e:
+    print("An error occurred:", e)
